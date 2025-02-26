c311acea89f87e3500f15e42123a9eaa7201ccf5
cinema-talkiez <cinema.talkiez.io@gmail.com>
2025-02-27 01:48:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-     
-    });
+    await mongoose.connect(process.env.MONGO_URI); // ✅ No need for extra options
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
